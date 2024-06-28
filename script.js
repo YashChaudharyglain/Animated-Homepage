37910b5976b712451ba144a041231eb3e8ce877e
Yash chaudhary <136463717+YashChaudharyglain@users.noreply.github.com>
2024-06-28 09:20:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function cursorEffect(){
+    var page1content=document.querySelector("#page1-content")
+    var cursor= document.querySelector("#cursor")
+   
+   
+    page1content.addEventListener("mousemove",function(dets){
+       gsap.to(cursor,{
+           x:dets.x,
+           y:dets.y
+       })
+   
+    })
+   
+   
+   
+    page1content.addEventListener("mouseenter",function(){
+      gsap.to(cursor,{
+       scale:1,opacity:1
+      })
+    })
+   
+    page1content.addEventListener("mouseleave",function(){
+       gsap.to(cursor,{
+           scale:0,opacity:0
+       })
+    })
+
+   
+
+
+}
+
+cursorEffect()
